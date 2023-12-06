4693286233e59d8d367ab64fd9fcd37b189c0ff4
Paulo Victor Duarte <paulovictor237@gmail.com>
2023-12-06 14:12:35 -0300

    react-native-reanimated

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Button, View} from 'react-native';
+import {Button, Text, View} from 'react-native';
+import Animated, {ZoomIn} from 'react-native-reanimated';
 
 export default function App() {
   return (
@@ -7,6 +8,11 @@ export default function App() {
       <View className="h-20 aspect-[3/1] rounded-lg border-2 border-gray-500" />
       <View className="mb-4" />
       <Button onPress={() => {}} title="clique aqui" />
+      <Animated.View
+        className={'bg-violet-500 rounded-lg p-4 mt-4'}
+        entering={ZoomIn.duration(2000).delay(500)}>
+        <Text>Animação</Text>
+      </Animated.View>
     </View>
   );
 }
